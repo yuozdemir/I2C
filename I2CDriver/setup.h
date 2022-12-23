2e9b5f8d5e40d83bacd94a2f26efeacef6af02eb
Yusuf Özdemir <103145392+yuozdemir@users.noreply.github.com>
2022-12-23 19:25:05 +0300

    Delete I2CDriver directory

--- I2CDriver/setup.h
+++
@@ -1,7 +0,0 @@
-#ifndef SETUP_H_
-#define SETUP_H_
-
-#include<33EP512MC806.h>                         //Mikrodenetleyicinin kütüphanesini tanýmla
-#use delay(internal = 16MHz)                     //Mikrodenetleyici için çalýþma frekansý tanýmla (DAHILI OSILATOR)
-
-#endif
